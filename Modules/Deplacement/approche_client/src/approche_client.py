cc17fe6f0e24f2cde3f674e353cc255a6ef9a76a
JuThe72 <97997557+JuThe72@users.noreply.github.com>
2024-03-21 16:01:01 -0400

    ObjectDetection, utiliser la fct projectPixelTo3dRay de l'objet PinholeCameraModel de image_geometry pour trouver coords 3D à partir de coords 2D pixels (uv). Bon, pour le .cpp, la fct donne le même résultat que faire le calcul avec la matrice P inv. Pour le script Python, la fct ne donne pas les bonnes coords 3D, donc utilisation du calcul avec la matrice P inv pour l'instant.

--- Modules/Deplacement/approche_client/src/approche_client.py
+++ Modules/Deplacement/approche_client/src/approche_client.py
@@ -8,6 +8,8 @@ from image_geometry import StereoCameraModel
 from homodeus_library.homodeus_precomp import *
 from base_navigation.NavSelector import NavSelector
 
+from cv_bridge import CvBridge, CvBridgeError
+from image_geometry import PinholeCameraModel
 import numpy as np
 
 
@@ -16,19 +18,22 @@ def f(a) -> None: #As we see, we can get events from the NavSelector in the cont
 
 
 class ApproachClient():
+  __CAMERA_INFO_TOPIC: str = '/xtion/rgb/camera_info'
+
   def __init__(self):
     initRosNode("approche_client")
 
-    self.__bridge = CvBridge()
+    self.__bridge   = CvBridge()
+    self.__depth_image = None
 
     # https://docs.ros.org/en/api/image_geometry/html/python/
-    self.__cameraModel = StereoCameraModel()  # TODO, utiliser cet objet a la place de tout faire tf 2D -> 3D a la main ...
-    # TODO, Recuperer info sur topic (Actuellement, coder a la dure. A NE PAS FAIRE)
-    # info camera intriseque : xtion/depth_registered/camera_info == /xtion/rgb/camera_info
-    self.__camera_P = [522.1910329546544, 0.0, 320.5, -0.0, 0.0, 522.1910329546544, 240.5, 0.0, 0.0, 0.0, 1.0, 0.0]
-    self.__camera_P = np.reshape(self.__camera_P, (3,4))
-    self.__camera_Pinv = np.linalg.pinv(self.__camera_P)
-    self.__depth_image = None
+    self.__model    = PinholeCameraModel()
+    self.__cam_info = rospy.wait_for_message(self.__CAMERA_INFO_TOPIC, CameraInfo, timeout=10)
+    self.__model.fromCameraInfo(self.__cam_info)
+
+    __camera_P = [522.1910329546544, 0.0, 320.5, -0.0, 0.0, 522.1910329546544, 240.5, 0.0, 0.0, 0.0, 1.0, 0.0]
+    __camera_P = np.reshape(__camera_P, (3,4))
+    self.__camera_Pinv = np.linalg.pinv(__camera_P)
 
     self.__navigator = NavSelector()
     self.__navigator.ConnectCallBack(f)
@@ -38,8 +43,8 @@ class ApproachClient():
     self.__tf_listener = tf.TransformListener()
 
     # JT, Utiliser ce subscriber si vous voulez utiliser les rosbags & pseudo_facedetection
-    # self.__camera_sub = rospy.Subscriber('/proc_output_depth_image', Float32MultiArray, self.__camera_callback, queue_size=5)
-    # self.__camera_sub = rospy.Subscriber('xtion/depth_registered/image_raw', Image, self.__camera_callback, queue_size=5)
+    # self.__camera_sub = rospy.Subscriber('/proc_output_depth_image', Float32MultiArray, self.__pseduo_camera_callback, queue_size=5)
+    self.__camera_sub = rospy.Subscriber('xtion/depth_registered/image_raw', Image, self.__camera_callback, queue_size=5)
 
     # JT, Utiliser ce subscriber si vous voulez utiliser les rosbags & pseudo_facedetection
     # self.__face_sub =rospy.Subscriber('proc_output_face_positions', FacePositions, self.__face_callback, queue_size=5)
@@ -49,42 +54,55 @@ class ApproachClient():
     rospy.loginfo("approach client initialized")
 
 
-  def __sendObjectDetection(self, what_is: str, start_pt: np.ndarray, end_pt: np.ndarray) -> None:
-    min_x             = start_pt.x if start_pt.x < end_pt.x else end_pt.x
-    max_x             = start_pt.x if start_pt.x > end_pt.x else end_pt.x
-    min_y             = start_pt.y if start_pt.y < end_pt.y else end_pt.y
-    max_y             = start_pt.y if start_pt.y > end_pt.y else end_pt.y
-    width             = max_x - min_x
-    height            = max_y - min_y
+  def __ownProjectPixelTo3dRay(self, ctr_pt:[], disparity: float) -> Vector3:
+    point_2d = point_2d = np.array([[ctr_pt[0], ctr_pt[1], 1]])
+    (x, y, z,_) = self.__camera_Pinv @ (point_2d * disparity).T
+    return Vector3(x[0], y[0], disparity)
+
+
+  def __prepareMsgObjectDetection(self, what_is: str, start_pt: [], end_pt: []) -> ObjectDetection:
+    up_left_pt: Point     = Point( start_pt[0] if start_pt[0] < end_pt[0] else end_pt[0], 
+                                   start_pt[1] if start_pt[1] < end_pt[1] else end_pt[1], 0 )
+    down_right_pt: Point  = Point( start_pt[0] if start_pt[0] > end_pt[0] else end_pt[0], 
+                                   start_pt[1] if start_pt[1] > end_pt[1] else end_pt[1], 0 )
+    width: int            = abs( start_pt[0] - end_pt[0] )
+    height: int           = abs( start_pt[1] - end_pt[1] )
+    padding_width: int    = int(width * 0.025)
+    padding_height: int   = int(height * 0.025)
 
     # Trouver la zone de l'image de profondeur associée a la zone de l'image rgb
-    face_depth_view   = self.__depth_image[min_y: max_y, min_x: max_x]
-    # TODO Ajouter une marge interne
+    face_depth_view   = self.__depth_image[( up_left_pt.y + padding_height ): ( down_right_pt.y - padding_height ),
+                                           ( up_left_pt.x + padding_width ): ( down_right_pt.x - padding_width ) ]
     object_dist: float  = np.nanmean(face_depth_view)
-    px = min_x + width / 2       # central point x (pixel)
-    py = min_y + height / 2      # central point y (pixel)
-    point_2d = np.array([[px, py, 1]])
 
-    # Initialize homodeus_msgs::ObjectDetection to send on topic
-    # TODO Revoir le calcul et d'où vient self.__camera_Pinv
-    # https://docs.ros.org/en/api/image_geometry/html/python/
-    # Commentaire 
-    # On initialise l’objet avec le message provenant du « camera_info » de la caméra, puis on peut utiliser des fonctions très pratiques comme « projectPixelTo3dRay », qui donne un vecteur unitaire qui lui ensuite peut être multiplié par le « d » que vous trouvez dans la matrice de profondeur. C’est qu’en fait la carte de profondeur est déjà en « z », donc on peut sauter une étape. Le détail est que ce n’est pas toujours clair dans quelles unités les données sont, mais l’avantage de passer par PinholeCameraModel est qu’il se base entièrement sur le format utilisé par camera_info et ne devrait pas vous donnez de soucis.
-    objectDetected: ObjectDetection = ObjectDetection()
-    (offset_x, offset_y,_,_) = self.__camera_Pinv @ (point_2d * object_dist).T
-    approach_point : Vector3 = Vector3(object_dist, -offset_x[0], -offset_y[0])
-    angle : float = atan2(-offset_x[0], object_dist)
-    euler2quat = tf.transformations.quaternion_from_euler( 0, 0, angle )
-    q : Quaternion = Quaternion(euler2quat[0],euler2quat[1],euler2quat[2],euler2quat[3])
+    # Trouver les coordonnées 3D a partir des coordonnées 2D pixels
+    center_pt: [] = [0,0]
+    center_pt[0]  = int(up_left_pt.x + width / 2)
+    center_pt[1]  = int(up_left_pt.y + height / 2)
+    point_3d      = self.__model.projectPixelTo3dRay(center_pt)
+    print('self.__model.projectPixelTo3dRay(center_pt).z', point_3d[2])
+    point_3d      = Point(point_3d[0] * object_dist, point_3d[1] * object_dist, point_3d[2] * object_dist)
+    # TODO puisque __model.projectPixelTo3dRay ne semble pas donne la bonne valeur (un z != 1 !?)
+    point_3d      = self.__ownProjectPixelTo3dRay(center_pt, object_dist)
+    print('point_3d\n', point_3d)
+    angle : float = atan2( point_3d.x, point_3d.z )
+
+    euler2quat    = tf.transformations.quaternion_from_euler( 0, 0, -angle )
+    q: Quaternion = Quaternion( euler2quat[0], euler2quat[1], euler2quat[2], euler2quat[3] )
 
     frame_from  = "/head_1_link"
     frame_to    = "/map"
     pose = PoseStamped()
-    pose.pose.position    = approach_point
+    pose.pose.position.x  =  point_3d.z
+    pose.pose.position.y  = -point_3d.x
+    pose.pose.position.z  = -point_3d.y
     pose.pose.orientation = q
     pose.header.stamp     = rospy.Time(0)
     pose.header.frame_id  = frame_from
     map_pose              = self.__tf_listener.transformPose(frame_to, pose)
+
+    # Initialize homodeus_msgs::ObjectDetection to send on topic
+    objectDetected: ObjectDetection = ObjectDetection()
 
     # Prepare msg header
     objectDetected.header.seq       = 0
@@ -92,18 +110,17 @@ class ApproachClient():
     objectDetected.header.frame_id  = what_is
 
     # Prepare msg box
-    objectDetected.box.x            = min_x   # start_pt.x if start_pt.x < end_pt.x else end_pt.x
-    objectDetected.box.y            = min_y   # start_pt.y if start_pt.y < end_pt.y else end_pt.y
-    objectDetected.box.width        = width   # abs(start_pt.x - end_pt.x)
-    objectDetected.box.height       = height  # abs(start_pt.y > end_pt.y)
+    objectDetected.box.x            = up_left_pt.x
+    objectDetected.box.y            = up_left_pt.y
+    objectDetected.box.width        = width   
+    objectDetected.box.height       = height  
 
     # Données géospatiales de l'objet détecté selon pose du robot
     objectDetected.distance         = object_dist
     objectDetected.angle            = angle
     objectDetected.pose             = map_pose.pose
 
-    # send to the topic
-    self.__object_detected_pub.publish(objectDetected)
+    return objectDetected
 
 
   # def __face_callback(self, detections : FacePositions) -> None:
@@ -119,24 +136,25 @@ class ApproachClient():
       min_x, max_x        = face.x, face.x + width
 
       object_dist   = object_detected.distance
+      # TODO Debug self.__prepareMsgObjectDetection
+      # self.__prepareMsgObjectDetection('person', start_pt=[min_x, min_y], end_pt=[max_x, max_y])
 
-      print("if", object_dist, '>', (1+self.__tolerance)*self.__approach_dist, "and 0 <", object_dist)
       if object_dist > (1+self.__tolerance)*self.__approach_dist and 0 < object_dist:
         dist_to_approach = object_dist - self.__approach_dist
 
-        px = min_x + width / 2       # central point x (pixel)
-        py = min_y + height / 2      # central point y (pixel)
-        point_2d = np.array([[px, py, 1]])
-        print('point_2d', point_2d)
+        center_pt: [] = [0,0]
+        center_pt[0]  = min_x + width / 2
+        center_pt[1]  = min_y + height / 2
+        point_3d      = self.__model.projectPixelTo3dRay(center_pt)
+        point_3d      = Point(point_3d[0] * dist_to_approach, point_3d[1] * dist_to_approach, point_3d[2] * dist_to_approach)
+        # TODO puisque __model.projectPixelTo3dRay ne semble pas donne la bonne valeur (un z != 1 !?)
+        point_3d      = self.__ownProjectPixelTo3dRay(center_pt, dist_to_approach)
 
-        (offset_x, offset_y,_,_) = self.__camera_Pinv @ (point_2d * dist_to_approach).T
-        print('poind_3d', offset_x[0], offset_y[0], dist_to_approach)
-
-        approach_point : Vector3 = Vector3(dist_to_approach, -offset_x[0], -offset_y[0]) # l'axe des x (profondeur) reste // au plan xy de /map
-        print('approach_point', approach_point.x, approach_point.y, approach_point.z)
-        angle : float = atan2(-offset_x[0], dist_to_approach)
-        print('angle', angle)
-        euler2quat = tf.transformations.quaternion_from_euler(0,0,angle)
+        approach_point : Vector3 = Vector3(dist_to_approach, -point_3d.x, -point_3d.y) # l'axe des x (profondeur) reste // au plan xy de /map
+        angle : float = atan2( point_3d.x, point_3d.z )
+        # print('approach_point', approach_point.x, approach_point.y, approach_point.z)
+        # print('angle', angle)
+        euler2quat = tf.transformations.quaternion_from_euler(0,0,-angle)
         q : Quaternion = Quaternion(euler2quat[0],euler2quat[1],euler2quat[2],euler2quat[3])
 
         # frame_from = "/xtion_rgb_optical_frame"     # l'axe des z (profondeur) change d'orientation en fct de la tete du robot
@@ -173,7 +191,7 @@ class ApproachClient():
 
 
   # Fct «callback» utilisee pour les rosbags & PseudoDepthImage
-  def __camera_callback(self, image : Float32MultiArray) -> None:
+  def __pseduo_camera_callback(self, image : Float32MultiArray) -> None:
     self.__height = image.layout.dim[0].size
     self.__width = image.layout.dim[1].size
     self.__depth_image = np.reshape(image.data, (self.__height, self.__width))
--- catkin_ws/src/pseudo_detection/src/pseudo_detection.cpp
+++ catkin_ws/src/pseudo_detection/src/pseudo_detection.cpp
@@ -89,9 +89,8 @@ static const std::string DETECTION_TOPIC  = "/homodeus/perception/detection";
 // Intrinsic parameters of the camera
 cv::Mat cameraIntrinsics;
 
-image_geometry::StereoCameraModel stereoCamera;
-
-cv::Mat camera_Pinv( cv::Size(4,3), CV_32F );
+// https://docs.ros.org/en/api/image_geometry/html/c++/
+image_geometry::PinholeCameraModel model_;
 
 // Our Action interface type for moving TIAGo's head, provided as a typedef for convenience
 // typedef actionlib::SimpleActionClient<control_msgs::PointHeadAction> PointHeadClient;
@@ -103,12 +102,14 @@ cv::Mat rgbImage;
 
 cv::Mat depthImage;
 
-ros::Publisher detection_pub;
-
 ros::Time latestImageStamp;
 
+ros::Publisher detection_pub;
+
 int drawed_rectanle = 0, detected_object = 0;
+
 cv::Point start_pnt, end_pnt;
+
 int choice_object_detected = 0;         // 0: person, 1: object
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -152,52 +153,36 @@ void depthCallback(const sensor_msgs::ImageConstPtr& imgMsg)
 void sendObjectDetection(ros::Time timestamp, std::string what_is, 
                          cv::Point start_pnt, cv::Point end_pnt)
 {
-  // Initialize homodeus_msgs::ObjectDetection to send on topic
-  homodeus_msgs::ObjectDetection objectDetected;
-  // Prepare msg header
-  objectDetected.header.seq       = 0;
-  objectDetected.header.stamp     = timestamp;
-  objectDetected.header.frame_id  = what_is;
-
-  // Prepare msg box
-  objectDetected.box.x            = start_pnt.x < end_pnt.x ? start_pnt.x : end_pnt.x;
-  objectDetected.box.y            = start_pnt.y < end_pnt.y ? start_pnt.y : end_pnt.y;
-  objectDetected.box.width        = std::abs(start_pnt.x - end_pnt.x);
-  objectDetected.box.height       = std::abs(start_pnt.y > end_pnt.y);
+  // 
+  cv::Point up_left_pnt           = cv::Point(start_pnt.x < end_pnt.x ? start_pnt.x : end_pnt.x, 
+                                              start_pnt.y < end_pnt.y ? start_pnt.y : end_pnt.y);
+  cv::Point down_right_pnt        = cv::Point(start_pnt.x > end_pnt.x ? start_pnt.x : end_pnt.x, 
+                                              start_pnt.y > end_pnt.y ? start_pnt.y : end_pnt.y);
+  int width                       = std::abs(start_pnt.x - end_pnt.x);
+  int height                      = std::abs(start_pnt.y - end_pnt.y);
+  int padding_width               = width * 0.025;
+  int padding_height              = height * 0.025;
 
   // Trouver la zone de l'image de profondeur associée a la zone de l'image rgb
-  cv::Rect traget_box( start_pnt, end_pnt );
-  // TODO Ajouter marge interne
+  cv::Rect traget_box( cv::Point( up_left_pnt.x + padding_width, up_left_pnt.y + padding_height ), 
+                       cv::Point( down_right_pnt.x - padding_width, down_right_pnt.y - padding_height ) );
   cv::Mat depth_box = depthImage( traget_box );
   cv::patchNaNs(depth_box, -1.);
   cv::Mat mask_not_nan = depth_box != -1.;
   float disparity = cv::mean( depthImage( traget_box ), mask_not_nan )[0];
 
-  // // TODO Stereo Camera initialized false, this fct below don't work
-  // https://docs.ros.org/en/api/image_geometry/html/c++/
-  // cv::Point center_pnt = cv::Point( objectDetected.box.x + objectDetected.box.width / 2, objectDetected.box.y + objectDetected.box.height / 2 );
-  // cv::Point3d positionPerson;
-  // stereoCamera.projectDisparityTo3d( center_pnt, disparity, positionPerson );
-  // ROS_INFO_STREAM( positionPerson );
-
   // Trouver les coordonnées 3D a partir des coordonnées 2D pixels
-  float cpnt[3] = { objectDetected.box.x + objectDetected.box.width / 2,
-                    objectDetected.box.y + objectDetected.box.height / 2, 1};
-  cv::Mat center_pnt_T( 3, 1, CV_32FC1, cpnt );
-  // TODO Est-ce que disparity est vraiment le Z ou plutot distance robot-objet direct 
-  cv::Mat point_3d = camera_Pinv * ( center_pnt_T * disparity );
-  float angle = std::atan2(-point_3d.at<float>( 0, 0 ), point_3d.at<float>( 2, 0 ));
-
-  // Données géospatiales de l'objet détecté selon pose du robot
-  objectDetected.distance         = disparity;
-  objectDetected.angle            = angle;
+  cv::Point center_pnt = cv::Point( up_left_pnt.x + width / 2, 
+                                    up_left_pnt.y + height / 2 );
+  // Pin Hole Camera
+  cv::Point3d point_3d = model_.projectPixelTo3dRay( center_pnt ) * disparity;
+  float angle = std::atan2( point_3d.x, point_3d.z );
 
   // Ce que la perception pourrait traiter de plus
   //  soit la pose de l'objet selon le robot
   //  ou la carte (prendre en consideration une rotation de la tete p/r au tronc)
-  // Ne fonctionne pas en cpp, mais le même code en Python fonctionne (voir approche_client)
   tf2::Quaternion q;
-  q.setRPY( 0, 0, angle );
+  q.setRPY( 0, 0, -angle );
   q.normalize();
 
   tf2_ros::Buffer tf_buffer;
@@ -210,19 +195,32 @@ void sendObjectDetection(ros::Time timestamp, std::string what_is,
   poseRelative.header.stamp       = latestImageStamp;
   poseRelative.header.frame_id    = "/head_1_link";
 
-  poseRelative.pose.position.x    =  point_3d.at<float>( 2, 0 );   // distance
-  poseRelative.pose.position.y    = -point_3d.at<float>( 0, 0 );   // offset x
-  poseRelative.pose.position.z    = -point_3d.at<float>( 1, 0 );   // offset y
+  poseRelative.pose.position.x    =  point_3d.z;   // distance objet-camera
+  poseRelative.pose.position.y    = -point_3d.x;   // offset x
+  poseRelative.pose.position.z    = -point_3d.y;   // offset y
   poseRelative.pose.orientation.x = q.getX();
   poseRelative.pose.orientation.y = q.getY();
   poseRelative.pose.orientation.z = q.getZ();
   poseRelative.pose.orientation.w = q.getW();
 
-  ROS_INFO_STREAM( poseRelative );
-  ROS_INFO_STREAM( objectDetected );
+  // Initialize homodeus_msgs::ObjectDetection to send on topic
+  homodeus_msgs::ObjectDetection objectDetected;
+  // Prepare msg header
+  objectDetected.header.seq       = 0;
+  objectDetected.header.stamp     = timestamp;
+  objectDetected.header.frame_id  = what_is;
+
+  // Prepare msg box
+  objectDetected.box.x            = up_left_pnt.x;
+  objectDetected.box.y            = up_left_pnt.y;
+  objectDetected.box.width        = width;
+  objectDetected.box.height       = height;
+
+  // Données géospatiales de l'objet détecté selon pose du robot
+  objectDetected.distance         = disparity;
+  objectDetected.angle            = angle;
+
   tf2::doTransform(poseRelative.pose, objectDetected.pose, head_1_link_to_map);
-  ROS_INFO_STREAM( poseRelative );
-  ROS_INFO_STREAM( objectDetected );
 
   detection_pub.publish( objectDetected );
 }
@@ -238,13 +236,13 @@ void onMouse( int event, int u, int v, int, void* )
   // Draw a rectangle on rgb image
   if ( event == cv::EVENT_LBUTTONDOWN && !drawed_rectanle )
   {
-    ROS_INFO("Start drawing a rectangle.");
+    // ROS_INFO("Start drawing a rectangle.");
     drawed_rectanle = 1;
     start_pnt = cv::Point(u, v);
   }
   else if ( drawed_rectanle )
   {
-    ROS_INFO("Continue drawing a rectangle.");
+    // ROS_INFO("Continue drawing a rectangle.");
     end_pnt = cv::Point(u, v);
     if ( choice_object_detected == 0 )
       cv::rectangle(rgbImage, start_pnt, end_pnt, CV_RGB(0, 255, 0), 2, 8, 0);
@@ -254,7 +252,7 @@ void onMouse( int event, int u, int v, int, void* )
 
     if ( event == cv::EVENT_LBUTTONUP )
     {
-      ROS_INFO("End drawing a rectangle.");
+      // ROS_INFO("End drawing a rectangle.");
       drawed_rectanle = 0;
       detected_object = ( cv::waitKey(50) != 27 ) ? 1 : 0;
     }
@@ -269,7 +267,6 @@ void onMouse( int event, int u, int v, int, void* )
   if(start_pnt == end_pnt)
     return;
 
-  // ROS_INFO_STREAM(cameraIntrinsics);
   std::string what_is = ( choice_object_detected == 0 ) ? "person" : "object";
   sendObjectDetection( latestImageStamp, what_is, start_pnt, end_pnt );
 }
@@ -315,25 +312,20 @@ int main(int argc, char** argv)
 
   // Get the camera intrinsic parameters from the appropriate ROS topic
   ROS_INFO("Waiting for camera intrinsics ... ");
-  sensor_msgs::CameraInfoConstPtr msg = ros::topic::waitForMessage
+  sensor_msgs::CameraInfoConstPtr cam_info = ros::topic::waitForMessage
       <sensor_msgs::CameraInfo>(CAMERA_INFO_TOPIC, ros::Duration(10.0));
-  if(msg.use_count() > 0)
+  if(cam_info.use_count() > 0)
   {
     cameraIntrinsics = cv::Mat::zeros(3,3,CV_64F);
-    cameraIntrinsics.at<double>(0, 0) = msg->K[0]; //fx
-    cameraIntrinsics.at<double>(1, 1) = msg->K[4]; //fy
-    cameraIntrinsics.at<double>(0, 2) = msg->K[2]; //cx
-    cameraIntrinsics.at<double>(1, 2) = msg->K[5]; //cy
+    cameraIntrinsics.at<double>(0, 0) = cam_info->K[0]; //fx
+    cameraIntrinsics.at<double>(1, 1) = cam_info->K[4]; //fy
+    cameraIntrinsics.at<double>(0, 2) = cam_info->K[2]; //cx
+    cameraIntrinsics.at<double>(1, 2) = cam_info->K[5]; //cy
     cameraIntrinsics.at<double>(2, 2) = 1;
   }
 
-  stereoCamera = image_geometry::StereoCameraModel();
-  ROS_INFO_STREAM( stereoCamera.initialized() );
-  float camera_intrinsics[12] = {  522.1910329546544, 0.0, 320.5, -0.0,
-                                     0.0, 522.1910329546544, 240.5, 0.0,
-                                     0.0, 0.0, 1.0, 0.0 };
-  cv::Mat camera_P( 3, 4, CV_32F, camera_intrinsics );
-  cv::invert(camera_P, camera_Pinv, cv::DECOMP_SVD);
+  model_.fromCameraInfo(cam_info);
+  ROS_INFO_STREAM("Model Camera is initialize ? " << model_.initialized() );
 
   // Create a point head action client to move the TIAGo's head
   // createPointHeadClient( pointHeadClient );
