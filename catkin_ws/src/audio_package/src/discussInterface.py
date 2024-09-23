3785f4952b5c792f272d1c2233f7156f52324bc3
tiblond <bera1107@usherbrooke.ca>
2024-09-23 06:35:18 -0400

    Copie d'interface pour discuss

---
+++ catkin_ws/src/audio_package/src/discussInterface.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import rospy
+from rospy import Publisher, Subscriber
+from homodeus_msgs.msg import HDResponse, HDDiscussionStarted, HDStatus
+from discuss import AudioHandler
+
+class AudioRosDiscuss():
+    _DISCUSS_REQUEST_TOPIC = "/Homodeus/Behaviour/Discuss/Request"
+    _DISCUSS_RESPONSE_TOPIC = "/Homodeus/Behaviour/Discuss/Response"
+    _DISCUSS_STATUS_TOPIC = "/Homodeus/Behaviour/Discuss/Status"
+    
+    def __init__(self, node_name: str = 'base_rotate') -> None:
+        # Initialise the node
+        rospy.init_node(node_name, anonymous=True)
+
+        # Initialise parameters
+        self.__discusser: AudioHandler = AudioHandler()
+        self.__desireID: int = 0
+
+        # Subscriber
+        self.__discuss_request_sub: Subscriber = Subscriber(self._DISCUSS_REQUEST_TOPIC, HDDiscussionStarted, self.__discuss_request_subscriber_callback)
+              
+        # Publisher
+        self.__discuss_status_pub: Publisher = Publisher(self._DISCUSS_STATUS_TOPIC, HDStatus, queue_size=1)
+        self.__discuss_response_pub: Publisher = Publisher(self._DISCUSS_RESPONSE_TOPIC, HDResponse, queue_size=1)
+
+        rospy.on_shutdown(self.__close_connection_node)
+        rospy.loginfo("Behavior Discuss initialized")
+    
+    def __discuss_request_subscriber_callback(self, msg: HDDiscussionStarted):
+        self.__desireID = msg.id.desire_id
+        rospy.loginfo("Request to discuss received")
+        self.__discusser.discuss(msg.message.data)
+        rospy.loginfo("Request to discuss finished")
+        response = HDResponse()
+        response.id.desire_id = self.__desireID
+        response.result = True
+        self.__discuss_response_pub.publish(response)
+        rospy.loginfo("Response to discuss sent")
+    
+    def __close_connection_node(self):
+        self.__discuss_request_sub.unregister()
+        self.__discuss_status_pub.unregister()
+        self.__discuss_response_pub.unregister()
+        rospy.loginfo("Closing Behavior Discuss node")
+
+if __name__ == '__main__':
+    try:
+        audio_ros_discuss = AudioRosDiscuss()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
