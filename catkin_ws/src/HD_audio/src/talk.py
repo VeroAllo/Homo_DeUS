1a23d9565818766aff9272cf2480d320dc70d681
JuThe72 <97997557+JuThe72@users.noreply.github.com>
2024-10-13 13:27:51 -0400

    Ajout msg TtsActionGoal en commentaire (En construction)

--- catkin_ws/src/HD_audio/src/talk.py
+++ catkin_ws/src/HD_audio/src/talk.py
@@ -10,3 +10,19 @@ class AudioTalk:
         tts = gTTS(text=initial_message, lang='en')
         tts.save("response.mp3")
         os.system("mpg321 response.mp3")
+
+# rostopic pub /tts/goal pal_interaction_msgs/TtsActionGoal "header:
+# seq: 0
+# stamp:
+# secs: 0
+# nsecs: 0
+# frame_id: ''
+# goal_id:
+# stamp:
+# secs: 0
+# nsecs: 0
+# id: ''
+# goal:
+# text:
+# rawtext:
+# text: 'Hello world'
