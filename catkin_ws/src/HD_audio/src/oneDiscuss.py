55ebfa2981eb35dbe5557d4ad004681b25e0dd55
JuThe72 <97997557+JuThe72@users.noreply.github.com>
2024-10-13 16:59:27 -0400

    Ajout de l'aiguillage du type de TTS dans oneDiscuss.py
    gTTS est par défaut et utilisera les haut-parleurs de la machine d'hôte tandis que l'option '--tts hdTTS' utilise le service TTS de pal, donc il faut que le ROS_MASTER aie parti ce service avant d'être capable d'utiliser hdTTS

--- catkin_ws/src/HD_audio/src/oneDiscuss.py
+++ catkin_ws/src/HD_audio/src/oneDiscuss.py
@@ -1,24 +1,34 @@
+#!/usr/bin/env python
+
+
+import argparse
 import json
 import vosk
-import rospy
-from std_msgs.msg import String
 import openai
 import pyaudio
 import threading
 import queue
 import os
 from gtts import gTTS
+
+import rospy
+from std_msgs.msg import String
+from hdTTS import hdTTS
 from homodeus_msgs.msg import HDResponse, HDDiscussionStarted, HDStatus
 
 class AudioRosDiscuss:
-    def __init__(self):
+    def __init__(self, tts_type:str):
+        self.tts_type = tts_type
+        if self.tts_type == 'hdTTS':
+            self.__tts = hdTTS()
+        else:
+            self.__sound_file:str = "response.mp3"
+
         self.audio_queue = queue.Queue()
         self.is_playing = threading.Event()
         self.stop_event = threading.Event()
         self.setup_audio()
         self.setup_ros()
-        
-
         
         self.message_history = [
             {"role": "system", "content": """
@@ -32,6 +42,25 @@ class AudioRosDiscuss:
             """}
         ]
         openai.api_key = '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'  # Assurez-vous que l'API key est définie ici
+
+    def __tts_prepare(self, text, lang):
+        if self.tts_type == 'hdTTS':
+            self.__tts.set_goal(text=text, lang=lang)
+        else:
+            tts = gTTS(text=text, lang=lang)
+            tts.save(self.__sound_file)
+
+    def __talk(self):
+        if self.tts_type == 'hdTTS':
+            self.__tts.talk_blocking()
+        else:
+            os.system("mpg321 {self.__sound_file}".format(self=self))
+
+    def __tts_talk(self, text, lang):
+        self.__tts_prepare(text, lang)
+        self.is_playing.set()
+        self.__talk()
+        self.is_playing.clear()
 
     def setup_audio(self):
         self.vosk_model = vosk.Model('vosk-model-small-en-us-0.15')  # Spécifiez le chemin correct ici
@@ -89,12 +118,8 @@ class AudioRosDiscuss:
                     # Extraire l'item sélectionné
                     selected_item = self.extract_order_item(response_text)
 
-
-                tts = gTTS(text=response_text, lang='en')
-                tts.save("response.mp3")
-                self.is_playing.set()
-                os.system("mpg321 response.mp3")
-                self.is_playing.clear()
+                # TODO, A valider si 'en_US' existe pour gTTS, car TtsAction est base sur RFC 3006
+                self.__tts_talk(response_text, 'en_US')
 
                 # Vérification de la fin de la conversation
                 if "thank you" in response_text.lower() or "have a pleasant meal" in response_text.lower():
@@ -116,6 +141,20 @@ class AudioRosDiscuss:
                 return item
         return None
 
+def add_parser():
+  parser = argparse.ArgumentParser(description='OneDiscuss')
+  parser.add_argument(
+    '--tts',
+    help='Set tts type',
+    default='gTTS',
+    type=str,
+    choices=['gTTS', 'hdTTS'],
+  )
+  args, unknown = parser.parse_known_args()
+  # unknown:=[__name, __log]
+  return args
+
 if __name__ == "__main__":
-    audio_ros_discuss = AudioRosDiscuss()
+    args = add_parser()
+    audio_ros_discuss = AudioRosDiscuss(args.tts)
     rospy.spin()
